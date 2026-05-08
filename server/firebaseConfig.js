8b74d6898fc985ef4b236bcf17c110dba040ce32
Aung Myo Kyaw <203843154+amkyawdev@users.noreply.github.com>
2026-05-08 17:02:41 +0630

    Update firebaseConfig.js

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -4,14 +4,14 @@
    =============================================== */
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyCcw0_7Pc4tIAapg-Hyvk4UtiQfo1USjU8",
-  authDomain:        "amk-apk.firebaseapp.com",
-  databaseURL:       "https://amk-apk-default-rtdb.firebaseio.com",
-  projectId:         "amk-apk",
-  storageBucket:     "amk-apk.firebasestorage.app",
-  messagingSenderId: "267632318274",
-  appId:             "1:267632318274:android:441e4b8cb66da60d04dadb",
-  measurementId:     "G-E2658JJ29R"
+  apiKey:            "",
+  authDomain:        "",
+  databaseURL:       "",
+  projectId:         "",
+  storageBucket:     "",
+  messagingSenderId: "",
+  appId:             "",
+  measurementId:     ""
 };
 
 // Initialize Firebase (safe to call multiple times)
